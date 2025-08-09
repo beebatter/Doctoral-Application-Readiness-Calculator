a32396a458144aac9fd30dda84e19eb7dff86e30
Kunwei Song <easkwon@outlook.com>
2025-08-09 20:29:17 +0100

    update the analysis of website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
  
 
 
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
